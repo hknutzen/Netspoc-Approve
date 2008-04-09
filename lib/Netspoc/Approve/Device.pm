5b50b3131ade2b3133e15ed5dc0ca3fc7a00e403
Heinz Knutzen <heinz.knutzen@gmx.de>
2008-04-09 22:46:24 +0000

    Moved generic part of method checkbanner from Firewall.pm to Device.pm

--- lib/Netspoc/Approve/Device.pm
+++ lib/Netspoc/Approve/Device.pm
@@ -574,6 +574,20 @@ sub checkidentity( $$ ) {
     return 1;
 }
 
+sub checkbanner($) {
+    my ($self) = @_;
+    if ( $self->{CHECKBANNER}
+     and $self->{PRE_LOGIN_LINES} !~ /$self->{CHECKBANNER}/)
+    {
+        if ($self->{APPROVE}) {
+            errpr "Missing banner at NetSPoC managed device.\n";
+        }
+        else {
+            warnpr "Missing banner at NetSPoC managed device.\n";
+        }
+    }
+}
+
 ##################################################################
 #    adaption layer
 ##################################################################
