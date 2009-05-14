41058ac3ae198a69f28b1bbc9048774c5f9c396f
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2009-05-14 07:51:16 +0000

    Fixed bug that marked ACLs as unchanged, although they had been changed.

--- lib/Netspoc/Approve/Cisco_FW.pm
+++ lib/Netspoc/Approve/Cisco_FW.pm
@@ -2220,8 +2220,8 @@ sub mark_as_unchanged {
     return if $parse_name eq 'CERT_ANCHOR';
     return if $parse_name eq 'DEFAULT_GROUP';
 
-    if ( not exists $self->{CHANGE}->{$parse_name} ) {
-	my $name = $parse_name eq 'ACCESS' ? 'ACL' : $parse_name;
+    my $name = $parse_name eq 'ACCESS' ? 'ACL' : $parse_name;
+    if ( not exists $self->{CHANGE}->{$name} ) {
 	$self->{CHANGE}->{$name} = 0;
     }
 }
