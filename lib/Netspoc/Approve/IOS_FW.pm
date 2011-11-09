4c17d0bffc2e76e9e661d88ec917e2ad22d855a1
Heinz Knutzen <heinz.knutzen@dataport.de>
2011-11-09 14:05:35 +0000

    Removed no longer needed sub check_firewall.

--- lib/Netspoc/Approve/IOS_FW.pm
+++ lib/Netspoc/Approve/IOS_FW.pm
@@ -20,33 +20,6 @@ sub version_drc2_IOS_FW() {
     return $id;
 }
 
-
-sub check_firewall ( $$ ) {
-    my ($self, $conf) = @_;
-
-    mypr "Check for CBAC at IOS firewall...\n";
-    my $cbac_ok = 1;
-    for my $intf (keys %{$conf->{IF}}){
-	next if($conf->{IF}->{$intf}->{SHUTDOWN});
-	mypr "   $intf";
-	if(exists $conf->{IF}->{$intf}->{SWITCHPORT}){
-	    mypr " - is a switchport - OK\n";
-	}
-	elsif($intf eq "Loopback0"){
-	    mypr " - is a loopback device - OK\n";
-	}
-	elsif(exists $conf->{IF}->{$intf}->{INSPECT}){
-	    mypr " - CBAC enabled - OK\n";
-	}
-	else{
-	    mypr " - no CBAC found\n";
-	    $cbac_ok = 0;
-	}
-    }
-    $cbac_ok or errpr "missing CBAC configuration\n";
-}
-
-
 # Packages must return a true value;
 1;
 
