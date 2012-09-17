8f839c3c18886b6baf64c8f1b6c6733a47e273e0
Heinz Knutzen <heinz.knutzen@dataport.de>
2012-09-17 10:33:06 +0200

    Added SUID support script "diamonds"

---
+++ suid-support/diamonds.c
@@ -0,0 +1,14 @@
+/* Wrapper script for approve.pl
+   This should be installed setuid and setgid 
+   to some dedicated user and group ID.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char program[] = "/usr/local/bin/approve.pl";
+
+int main( int argc, char *argv[]) 
+{
+    return execv(program, argv); 
+} 
