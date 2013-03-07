2568830fe4fc54679fe60987a84aa41327d405a1
Heinz Knutzen <heinz.knutzen@dataport.de>
2013-03-07 17:29:47 +0100

    Prefix calls to "sh users", "sh tcp .." with "do"
    This is necessary because we enter conf mode earlier now.

--- lib/Netspoc/Approve/IOS.pm
+++ lib/Netspoc/Approve/IOS.pm
@@ -799,7 +799,9 @@ sub get_my_connection {
     # Output seen from IOS 12.4(3f):
     # * vty 322      netspoc   idle                 00:00:00 10.11.12.13
     # ==> take first number as vty and IP at end of line.
-    my $lines = $self->get_cmd_output('sh users | incl ^\*');
+    my $cmd = 'sh users | incl ^\*';
+    $cmd = "do $cmd" if $self->check_conf_mode() && !$self->{COMPARE};
+    my $lines = $self->get_cmd_output($cmd);
     my $line = $lines->[0];
     chomp $line;
     my ($vty, $s_ip);
@@ -812,7 +814,9 @@ sub get_my_connection {
     my $src_ip = quad2int($s_ip) or abort("Can't parse src ip: $s_ip");
 
     # Read tcp details for my connection.
-    $lines = $self->get_cmd_output("sh tcp $vty | incl Local host:");
+    $cmd = "sh tcp $vty | incl Local host:";
+    $cmd = "do $cmd" if $self->check_conf_mode() && !$self->{COMPARE};
+    $lines = $self->get_cmd_output($cmd);
     $line = $lines->[0];
     my ($port, $d_ip);
     if ($line =~ /Local host:\s([\d.]+),\sLocal port:\s(\d+)/i) {
