c43916c7f4bfacc4f8fde4d99fff0897e4699412
Heinz Knutzen <heinz.knutzen@dataport.de>
2015-07-21 13:50:13 +0200

    Fix: Remove crypto filter ACL in conf mode.

--- lib/Netspoc/Approve/IOS.pm
+++ lib/Netspoc/Approve/IOS.pm
@@ -737,20 +737,20 @@ sub compare_crypto_map_entries {
                 and next;
         }
 
+        $self->enter_conf_mode();
+
         # Add ACL to device.
         if ($spoc_acl) {
 
             # Add new ACL
             $self->schedule_reload(5);
             info("Creating new ACL");
-            $self->enter_conf_mode();
             my $new_acl = $self->define_acl($conf, $spoc, $spocacl_name);
 
             # Assign new acl to crypto map
             info("Assigning $new_acl");
             $self->cmd("crypto map $conf_entry->{name} $sequ");
             $self->cmd("set ip access-group $new_acl $in_out");
-            $self->leave_conf_mode();
             $self->cancel_reload();
         }
         
@@ -764,6 +764,7 @@ sub compare_crypto_map_entries {
             }
             $self->remove_acl($conf_acl);
         }
+        $self->leave_conf_mode();
     }
 }
 
