3f46a8ebf56dae02bccbb635db219a2da6361598
Heinz Knutzen <heinz.knutzen@dataport.de>
2017-05-08 15:09:14 +0200

    Must not autovivify {CRYPTO}.

--- lib/Netspoc/Approve/Cisco_Router.pm
+++ lib/Netspoc/Approve/Cisco_Router.pm
@@ -6,7 +6,7 @@ Base class for Cisco routers (IOS, NX-OS)
 =head1 COPYRIGHT AND DISCLAIMER
 
 https://github.com/hknutzen/Netspoc-Approve
-(c) 2016 by Heinz Knutzen <heinz.knutzen@gmail.com>
+(c) 2017 by Heinz Knutzen <heinz.knutzen@gmail.com>
 (c) 2011 by Daniel Brunkhorst <daniel.brunkhorst@web.de>
 
 This program is free software; you can redistribute it and/or modify
@@ -390,10 +390,8 @@ sub align_vrfs {
         $vrf ||= 'global';
         info("Leaving VRF $vrf untouched");
     }
-    if (keys %removed) {
-        keys %{ $spoc->{CRYPTO} } and
-            abort("Crypto and VRF can't be used together," .
-                  " if some VRF is unmanaged");
+    if (keys %removed and grep { /^CRYPTO/ } keys %$spoc) {
+        abort("Crypto and VRF can't be used together, if some VRF is unmanaged");
     }
 }
 
