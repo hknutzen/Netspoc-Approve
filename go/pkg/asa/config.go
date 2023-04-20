79c1001cc79eefc73ea77ecf031629c779f35d1a
Heinz Knutzen <heinz.knutzen@dataport.de>
2023-04-20 11:55:03 +0200

    Added MergeSpoc

--- go/pkg/asa/config.go
+++ go/pkg/asa/config.go
@@ -4,8 +4,20 @@ import "github.com/hknutzen/Netspoc-Approve/go/pkg/device"
 
 func (n1 *ASAConfig) MergeSpoc(c2 device.DeviceConfig) device.DeviceConfig {
 	n2 := c2.(*ASAConfig)
-	if n2 == nil {
-		return n1
+	lookup := n1.lookup
+	if lookup == nil {
+		lookup = make(map[string]map[string][]*cmd)
+		n1.lookup = lookup
+	}
+	for prefix, m2 := range n2.lookup {
+		m1 := lookup[prefix]
+		if m1 == nil {
+			m1 = make(map[string][]*cmd)
+			lookup[prefix] = m1
+		}
+		for name, l := range m2 {
+			m1[name] = append(m1[name], l...)
+		}
 	}
 	return n1
 }
