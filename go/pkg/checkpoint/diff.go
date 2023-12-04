0fc691e600704e5a300ab0ca89f75c5f1f155765
Heinz Knutzen <heinz.knutzen@dataport.de>
2023-12-04 11:10:01 +0100

    Use "github.com/pkg/diff/myers"

--- go/pkg/checkpoint/diff.go
+++ go/pkg/checkpoint/diff.go
@@ -6,10 +6,22 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hknutzen/Netspoc-Approve/go/pkg/device"
+	"github.com/pkg/diff/myers"
 	"golang.org/x/exp/slices"
 )
 
 type jsonMap map[string]interface{}
+
+type rulesPair struct {
+	aRules, bRules []*chkpRule
+}
+
+func (ab *rulesPair) LenA() int { return len(ab.aRules) }
+func (ab *rulesPair) LenB() int { return len(ab.bRules) }
+
+func (ab *rulesPair) Equal(ai, bi int) bool {
+	return ab.aRules[ai].Name == ab.bRules[bi].Name
+}
 
 func diffConfig(a, b *chkpConfig) []change {
 	var changes []change
@@ -41,111 +53,104 @@ func diffConfig(a, b *chkpConfig) []change {
 			aObj.setDeletable()
 		}
 	}
-	aRuleMap := make(map[string]*chkpRule)
-	for _, r := range a.Rules {
-		aRuleMap[r.Name] = r
+	ab := &rulesPair{
+		aRules: a.Rules,
+		bRules: b.Rules,
 	}
-	// Find postion for added rules from Netspoc,
-	// - add before exiting rule on device or
-	// - add at bottom of ruleset.
-	addIdx := -1
-	for i, bRule := range b.Rules {
-		if aRule, found := aRuleMap[bRule.Name]; found {
-			if addIdx != -1 {
-				pos := jsonMap{"before": aRule.Name}
-				for j := addIdx; j < i; j++ {
-					b.Rules[j].Position = pos
-				}
-				addIdx = -1
-			}
-		} else if addIdx == -1 {
-			addIdx = i
-		}
-	}
-	if addIdx != -1 {
-		pos := "bottom"
-		for j := addIdx; j < len(b.Rules); j++ {
-			b.Rules[j].Position = pos
-		}
-	}
-	for _, bRule := range b.Rules {
-		if aRule, found := aRuleMap[bRule.Name]; found {
-			aRule.needed = true
-			changed := make(jsonMap)
-			compareObjects := func(attr string, aL, bL []chkpName) {
-				aMap := getNameMap(aL)
-				bMap := getNameMap(bL)
-				var add []chkpName
-				var remove []chkpName
-				for _, aName := range aL {
-					if !bMap[aName] {
-						remove = append(remove, aName)
-						markDeletable(aName)
+	diff := myers.Diff(nil, ab).Ranges
+	for _, r := range diff {
+		if r.IsDelete() {
+			// Remove unneeded rules from device.
+			for _, aRule := range a.Rules[r.LowA:r.HighA] {
+				setDeletable := func(l []chkpName) {
+					for _, n := range l {
+						markDeletable(n)
 					}
 				}
-				for _, bName := range bL {
-					if !aMap[bName] {
-						add = append(add, bName)
-					}
-				}
-				if add != nil && remove != nil {
-					// Replace all if elements are both added and removed.
-					changed[attr] = bL
-				} else if add != nil {
-					changed[attr] = map[string][]chkpName{"add": add}
-				} else if remove != nil {
-					changed[attr] = map[string][]chkpName{"remove": remove}
-				}
-			}
-			if aRule.Comments != bRule.Comments {
-				changed["comments"] = bRule.Comments
-			}
-			if aRule.Action != bRule.Action {
-				changed["action"] = bRule.Action
-			}
-			compareObjects("source", aRule.Source, bRule.Source)
-			compareObjects("destination", aRule.Destination, bRule.Destination)
-			compareObjects("service", aRule.Service, bRule.Service)
-			if !slices.Equal(aRule.InstallOn, bRule.InstallOn) {
-				changed["install-on"] = bRule.InstallOn
-			}
-			if len(changed) > 0 {
-				changed["name"] = bRule.Name
-				changed["layer"] = bRule.Layer
-				// TODO: Set correct position.
-				data, _ := json.Marshal(changed)
+				setDeletable(aRule.Source)
+				setDeletable(aRule.Destination)
+				setDeletable(aRule.Service)
+				data, _ := json.Marshal(
+					chkpRuleID{Name: aRule.Name, Layer: "network"})
 				changes = append(changes,
 					change{
-						endpoint: "set-access-rule",
+						endpoint: "delete-access-rule",
 						postData: data,
 					})
 			}
-		} else {
-			data, _ := json.Marshal(bRule)
-			changes = append(changes,
-				change{
-					endpoint: "add-access-rule",
-					postData: data,
-				})
-		}
-	}
-	// Remove unneeded rules from device.
-	for _, aRule := range a.Rules {
-		if !aRule.needed {
-			setDeletable := func(l []chkpName) {
-				for _, n := range l {
-					markDeletable(n)
+		} else if r.IsInsert() {
+			// Add rules from Netspoc
+			// - add before exiting rule on device or
+			// - add at bottom of ruleset.
+			var pos interface{}
+			if r.LowA < len(a.Rules) {
+				pos = jsonMap{"before": a.Rules[r.LowA].Name}
+			} else {
+				pos = "bottom"
+			}
+			for _, bRule := range b.Rules[r.LowB:r.HighB] {
+				bRule.Position = pos
+				data, _ := json.Marshal(bRule)
+				changes = append(changes,
+					change{
+						endpoint: "add-access-rule",
+						postData: data,
+					})
+			}
+		} else if r.IsEqual() {
+			// Change attributes of rule remaining at same position.
+			for i, aRule := range a.Rules[r.LowA:r.HighA] {
+				bRule := b.Rules[r.LowB:r.HighB][i]
+				aRule.needed = true
+				changed := make(jsonMap)
+				compareObjects := func(attr string, aL, bL []chkpName) {
+					aMap := getNameMap(aL)
+					bMap := getNameMap(bL)
+					var add []chkpName
+					var remove []chkpName
+					for _, aName := range aL {
+						if !bMap[aName] {
+							remove = append(remove, aName)
+							markDeletable(aName)
+						}
+					}
+					for _, bName := range bL {
+						if !aMap[bName] {
+							add = append(add, bName)
+						}
+					}
+					if add != nil && remove != nil {
+						// Replace all if elements are both added and removed.
+						changed[attr] = bL
+					} else if add != nil {
+						changed[attr] = map[string][]chkpName{"add": add}
+					} else if remove != nil {
+						changed[attr] = map[string][]chkpName{"remove": remove}
+					}
+				}
+				if aRule.Comments != bRule.Comments {
+					changed["comments"] = bRule.Comments
+				}
+				if aRule.Action != bRule.Action {
+					changed["action"] = bRule.Action
+				}
+				compareObjects("source", aRule.Source, bRule.Source)
+				compareObjects("destination", aRule.Destination, bRule.Destination)
+				compareObjects("service", aRule.Service, bRule.Service)
+				if !slices.Equal(aRule.InstallOn, bRule.InstallOn) {
+					changed["install-on"] = bRule.InstallOn
+				}
+				if len(changed) > 0 {
+					changed["name"] = bRule.Name
+					changed["layer"] = bRule.Layer
+					data, _ := json.Marshal(changed)
+					changes = append(changes,
+						change{
+							endpoint: "set-access-rule",
+							postData: data,
+						})
 				}
 			}
-			setDeletable(aRule.Source)
-			setDeletable(aRule.Destination)
-			setDeletable(aRule.Service)
-			data, _ := json.Marshal(chkpRuleID{Name: aRule.Name, Layer: "network"})
-			changes = append(changes,
-				change{
-					endpoint: "delete-access-rule",
-					postData: data,
-				})
 		}
 	}
 	// Remove unneeded objects from device.
