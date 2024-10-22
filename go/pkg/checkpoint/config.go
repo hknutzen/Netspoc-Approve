c921dd4ea6fdb250fa1de93e6cfba56c8ff58219
Heinz Knutzen <heinz.knutzen@dataport.de>
2024-10-22 12:32:14 +0200

    Support raw file

--- go/pkg/checkpoint/config.go
+++ go/pkg/checkpoint/config.go
@@ -1,7 +1,49 @@
 package checkpoint
 
-import "github.com/hknutzen/Netspoc-Approve/go/pkg/deviceconf"
+import (
+	"slices"
 
-func (n1 *chkpConfig) MergeSpoc(c2 deviceconf.Config) deviceconf.Config {
-	return n1
+	"github.com/hknutzen/Netspoc-Approve/go/pkg/deviceconf"
+)
+
+func (a *chkpConfig) MergeSpoc(d deviceconf.Config) deviceconf.Config {
+	b := d.(*chkpConfig)
+	a.Networks = append(a.Networks, b.Networks...)
+	a.Hosts = append(a.Hosts, b.Hosts...)
+	a.Groups = append(a.Groups, b.Groups...)
+	a.TCP = append(a.TCP, b.TCP...)
+	a.UDP = append(a.UDP, b.UDP...)
+	a.ICMP = append(a.ICMP, b.ICMP...)
+	a.ICMP6 = append(a.ICMP6, b.ICMP6...)
+	a.SvOther = append(a.SvOther, b.SvOther...)
+	// Add rules.
+	// Rules are prepended per default.
+	// Rules with attribute .Append are appended after last non Drop line.
+	var prependACL, appendACL []*chkpRule
+	for _, ru := range b.Rules {
+		if ru.Append {
+			ru.Append = false
+			appendACL = append(appendACL, ru)
+		} else {
+			prependACL = append(prependACL, ru)
+		}
+	}
+	if len(prependACL) > 0 {
+		a.Rules = append(prependACL, a.Rules...)
+	}
+	if len(appendACL) > 0 {
+		// Find last non Drop line.
+		i := len(a.Rules) - 1
+		for ; i >= 0; i-- {
+			if a.Rules[i].Action != "Drop" {
+				i++
+				break
+			}
+		}
+		a.Rules = slices.Insert(a.Rules, i, appendACL...)
+	}
+	for gw, lb := range b.GatewayRoutes {
+		a.GatewayRoutes[gw] = append(a.GatewayRoutes[gw], lb...)
+	}
+	return a
 }
